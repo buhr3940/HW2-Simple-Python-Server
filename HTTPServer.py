8fdc91e40e7a563bc1a82cbde2ee1b5660956069
buhr3940 <buhr3940@stthomas.edu>
2015-02-18 20:12:32 -0600

    HW2_buhr3940
    Homework #2 Simple Python Server - Finalized files

--- HTTPServer.py
+++ HTTPServer.py
@@ -8,7 +8,7 @@ from os import curdir, sep
 
 #Define some variables to be used in the execution of the program
 HOST_NAME = '' # can be 'localhost' or if you change your hosts.txt file, what happens?? ;)
-PORT_NUMBER = 9001 #If you kill the server un-gracefully you may need to change this to an open socket.
+PORT_NUMBER = 80 #If you kill the server un-gracefully you may need to change this to an open socket.
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 #MyHandler class implements standard standard HTTP menthods
@@ -20,31 +20,55 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         #Respond to a GET request.
         try:
-            if self.path.endswith((".html",".htm")):
-                f = open(curdir + sep + self.path) #self.path has /index.html
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
+			if self.path == "/":
+				f = open(curdir + sep + "index.html") #self.path has /index.html
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
                 # Start sending content
-                self.wfile.write(f.read())
-                f.close()
-                return
+				self.wfile.write(f.read())
+				f.close()
+				return
+				
+			if self.path.endswith((".html",".htm")):
+				f = open(curdir + sep + self.path) #self.path has /index.html
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+                # Start sending content
+				self.wfile.write(f.read())
+				f.close()
+				return
                 
-            if self.path.endswith(".time"):
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
+			if self.path.endswith(".time"):
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
                 # Send current time
-                self.wfile.write("Are the chilren all in bed because now its " + str(time.localtime()[3]) + " O'Clock!!") 
-                return
+				self.wfile.write("Are the children all in bed because now its " + str(time.localtime()[3]) + " O'Clock!!") 
+				return
                 
-            if self.path.endswith(".gif"):
-                # IMPLEMENT THIS
-                return
+			if self.path.endswith(".gif"):
+				f=open(curdir + sep + self.path, 'rb')
+				self.send_response(200)
+				self.send_header("Content-type", "image/gif")
+				self.end_headers()
+                # Start sending content
+				self.wfile.write(f.read())
+				f.close()
+				return
+           
                 
-            if self.path.endswith(".jpg"):
-                # IMPLEMENT THIS
-                return
+			if self.path.endswith(".jpg"):
+				f=open(curdir + sep + self.path, 'rb')
+				self.send_response(200)
+				self.send_header("Content-type", "image/jpg")
+				self.end_headers()
+                # Start sending content
+				self.wfile.write(f.read())
+				f.close()
+				return
+				
         except IOError:
             self.send_error(404,'File not found: %self' % self.path)
             
